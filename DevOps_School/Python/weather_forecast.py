d44f2381061cb005d5f9efdd21b8c74574680c75
Serhii Pytliak <pytliak.serhii@gmail.com>
2022-06-08 18:51:53 +0300

    added script for weather forecast

---
+++ DevOps_School/Python/weather_forecast.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys, os
+from pyowm.owm import OWM
+from pyowm.utils.config import get_default_config
+
+def get_weather(city, APIID):
+    try:
+        config_dict = get_default_config()
+        config_dict['language'] = 'eng'
+        owm = OWM(APIID, config_dict)
+        mgr = owm.weather_manager()
+        
+        observation = mgr.weather_at_place(city)
+        w = observation.weather
+        
+        temperature = w.temperature('celsius')['temp']
+        wind = w.wind()['speed']
+        humidity = w.humidity
+
+        print(
+            'The weather in the {} is: \n'.format(city.upper()) +
+            'temperature: {} °C \n'.format(temperature) +
+            'wind: {} m/s \n'.format(wind) +
+            'humidity: {} % \n'.format(humidity)
+        )
+
+    except Exception as e:
+        print("Error: " + str(e))
+
+def main():
+    APIID = os.environ.get('APIID')
+    # check APIID env
+    if "APIID" in os.environ: 
+        pass
+    else:
+        exit('Environment variable APIID (API key) does not exist. Please export APIID in your env')
+
+    # check city
+    if len(sys.argv) > 1:
+       city = sys.argv[1]
+    else: 
+        city = input("Please enter a city: ")
+    
+    get_weather(city, APIID)
+
+
+if __name__ == '__main__':
+    main()
